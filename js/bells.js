b76ac73054ed74898e1af9a72d4634427ff15f56
Sami Glasco <samiglasco@gmail.com>
2016-01-06 14:06:14 -0600

    updated bell audio to webaudio

--- js/bells.js
+++ js/bells.js
@@ -1,8 +1,16 @@
-function playAudio(name) {
-    document.getElementById(name).currentTime = 0;
-    document.getElementById(name).play();
+function playAudio(id) {
+    if ((navigator.userAgent.toLowerCase().indexOf('chrome') <= -1)) {
+        sounds[id].play();
+    } else {
+        document.getElementById(id).play();
+    }
+
 }
 
-function pauseAudio(name){
-    document.getElementById(name).pause();
+var sounds = new Array(3);
+function initAudio() {
+    sounds[0] = new WebAudioAPISound("sounds/Big-Temple-Bell");
+    sounds[1] = new WebAudioAPISound("sounds/Japanese-Temple-Bell-Small");
+    sounds[2] = new WebAudioAPISound("sounds/Temple-Bell");
+    sounds[3] = new WebAudioAPISound("sounds/Zen-Temple-Bell");
 }
